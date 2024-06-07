40f5d1f990f3cfaface211225538267981be038a
sebasbas77 <michaellop77@outlook.es>
2024-06-07 13:56:08 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -33,15 +33,15 @@ function formatAddress(data) {
 function updateCompleteAddress() {
     const dateTime = document.getElementById('datetime').value;
     const coordinates = document.getElementById('coordinates').value;
-    const address = document.getElementById('address').value;
     const group = document.getElementById('group').value.trim();
     const contractor = document.getElementById('contractor').value.trim();
 
-    const lines = address.split('\n');
-    const addressLines = lines.slice(2).filter(line => line.trim() !== ''); // Keep only non-empty address lines
-    const updatedAddress = `${dateTime}\n${coordinates}\n${addressLines.join('\n')}\n${group}\n${contractor}`;
+    const address = document.getElementById('address').value.split('\n');
 
-    document.getElementById('address').value = updatedAddress;
+    const newAddress = address.slice(0, address.findIndex(line => line.startsWith('PRO-') || line.startsWith('PROCISA')));
+    const updatedAddress = [...newAddress, group, contractor].filter(line => line.trim() !== '').join('\n');
+
+    document.getElementById('address').value = `${dateTime}\n${coordinates}\n${updatedAddress}`;
 }
 
 function showPosition(position) {
